946070797b04c0429c60d2218fb852c33efacb71
Grace-254 <gracekavutu6@gmail.com>
2026-01-29 14:41:17 +0300

    Add Task 3 authentication views, URLs, and templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
@@ -29,3 +31,20 @@ class UserRegisterView(CreateView):
     form_class = UserCreationForm
     template_name = "relationship_app/register.html"
     success_url = reverse_lazy("login")
+class UserLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+
+
+class UserLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
+
+
+class UserRegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = "relationship_app/register.html"
+    success_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
